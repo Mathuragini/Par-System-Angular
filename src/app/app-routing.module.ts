8f8a567e95e9d164c6c32a6adfcb81bd9c2f6991
Mathuragini <mathusharma15@gmail.com>
2019-07-17 12:41:18 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ScheduleparSingleComponent } from "./component/schedulepar-single/schedulepar-single.component";
+import { EvaluateParComponent } from "./component/evaluate-par/evaluate-par.component";
+import { ScoreMyparComponent } from "./component/score-mypar/score-mypar.component";
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: "schedulepar_single", component: ScheduleparSingleComponent },
+  { path: "evaluatepar", component: EvaluateParComponent },
+  { path: "scorepar", component: ScoreMyparComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const routingComponents = [
+  ScheduleparSingleComponent,
+  EvaluateParComponent,
+  ScoreMyparComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { DashboardComponent } from './shared/dashboard/dashboard.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ScheduleparSingleComponent } from './component/schedulepar-single/schedulepar-single.component';
-import { ScoreMyparComponent } from './component/score-mypar/score-mypar.component';
-import { EvaluateParComponent } from './component/evaluate-par/evaluate-par.component';
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SideBarComponent } from "./shared/side-bar/side-bar.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { DashboardComponent } from "./shared/dashboard/dashboard.component";
+import { HeaderComponent } from "./shared/header/header.component";
+// import { ScheduleparSingleComponent } from "./component/schedulepar-single/schedulepar-single.component";
+// import { ScoreMyparComponent } from "./component/score-mypar/score-mypar.component";
+// import { EvaluateParComponent } from "./component/evaluate-par/evaluate-par.component";
 
 @NgModule({
   declarations: [
@@ -18,15 +18,13 @@ import { EvaluateParComponent } from './component/evaluate-par/evaluate-par.comp
     FooterComponent,
     DashboardComponent,
     HeaderComponent,
-    ScheduleparSingleComponent,
-    ScoreMyparComponent,
-    EvaluateParComponent
+    // ScheduleparSingleComponent,
+    // ScoreMyparComponent,
+    // EvaluateParComponent
+    routingComponents // [ScheduleparSingleComponent + ScoreMyparComponent + EvaluateParComponent]
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
